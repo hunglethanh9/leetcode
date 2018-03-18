91feaf1025d3d274f9d9b2a359f938a70a66bf97
灰风GreyWind <xiezhq3@mail2.sysu.edu.cn>
2018-03-18 10:36:27 +0800

    Create 217. Contains Duplicate.js

---
+++ 217. Contains Duplicate/217. Contains Duplicate.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    for(var i = 0; i < nums.length; i++) {
+        if(nums.indexOf(nums[i]) !== i) return true;
+    }
+    return false;
+};
