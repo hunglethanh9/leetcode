e99318ed357bd456b6692ac5be2b35ce33e32614
灰风GreyWind <xiezhq3@mail2.sysu.edu.cn>
2018-03-19 10:28:46 +0800

    Update 796. Rotate String.js

--- 796. Rotate String/796. Rotate String.js
+++ 796. Rotate String/796. Rotate String.js
@@ -1,21 +1,27 @@
-# @param {String} a
-# @param {String} b
-# @return {Boolean}
-def rotate_string(a, b)
-	return false if a.size != b.size
-	a, b = a.split(""), b.split("")
-	result = true
-	(a.size + 1).times do |i|
-		result = true
-		a.each_with_index do |val, index|
-			if val != b[(i + index) % a.size]
-				result = false
-				break
-			end
-		end
-		if result
-			return result
-		end
-	end
-	result
-end
+/**
+ * @param {string} A
+ * @param {string} B
+ * @return {boolean}
+ */
+var rotateString = function(A, B) {
+    if (A.length !== B.length) return false;
+    
+    var size = A.length;
+    
+    if(size === 0) return true;
+    
+    for(var i = 0; i < A.length; i++) {
+        var result = true
+        
+        for(var j = 0; j < A.length; j++) {
+            if(A[j] !== B[(j + i) % size]) {
+                result = false;
+                break;
+            }
+        }
+        
+        if(result) return true;
+    }
+    
+    return false;
+};
